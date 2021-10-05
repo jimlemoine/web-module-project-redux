6212a72297c01e443e09b602dcbc60af32d9263d
jimlemoine <jlemoine007@gmail.com>
2021-10-05 12:28:17 -0700

    done connecting movie reducer

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider} from 'react-redux';
 
-import reducer from './reducers';
+import movieReducer from './reducers';
 
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
-const store = createStore(reducer);
+const store = createStore(movieReducer);
 
 ReactDOM.render(
-  <Provider >
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
