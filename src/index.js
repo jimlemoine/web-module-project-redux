72a4f1078bb683477101e2b4750395fe2b2494fd
jimlemoine <jlemoine007@gmail.com>
2021-10-05 11:56:55 -0700

    set up redux and set default initial state complete

--- src/index.js
+++ src/index.js
@@ -9,9 +9,13 @@ import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <Router>
+  <Provider >
+    <Router>
       <App />
-  </Router>,
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
